2410c992db11a9f6f28e2a2396c2b58cf21106b2
nsokolowska <natalia.sokolowska12@gmail.com>
2023-03-04 17:39:20 +0000

    Initial commit

---
+++ Basic C# Submission Assignment/Acme Accounting Systems/Acme Accounting Systems/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace Acme_Accounting_Systems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Acme Accounting System");
+            Console.ReadLine();
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SampleApplication.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace SampleApplication
+{
+    class Starter {
+        public static void Main(string[] args)
+        {
+            Console.Title = "First Project";
+            Console.WriteLine("Hello, World");
+
+            Console.ReadKey();
+        }
+    }
+}
