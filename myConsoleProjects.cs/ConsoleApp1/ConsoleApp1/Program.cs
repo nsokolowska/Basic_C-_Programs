cc7e82920bca80f931488270db5cdbc0bd3dc363
nsokolowska <natalia.sokolowska12@gmail.com>
2023-03-05 13:31:31 +0000

    Create Program.cs

---
+++ myConsoleProjects.cs/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   //string
+            //    Console.WriteLine("What is your name?");
+            //    string yourname = Console.ReadLine(); 
+            //    Console.WriteLine("Your name is " + yourname);
+            //    Console.ReadLine();
+
+
+            //Plus 5
+
+            //Console.WriteLine("What is your favourite number?");
+            //string favouriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favouriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favourite numeber plus 5 is " + total);
+            //Console.ReadLine();
+
+            //BOOL
+            bool isStuding = false;
+            byte hoursWroked = 42;
+            sbyte currentTemperature = -23;
+            char questionmark = '\u2103';
+
+            decimal moneyInBank = 100.5m;
+
+            double heightInCentimeters = 211.30202093;
+
+            float secondsLeft = 2.63f;
+
+            short temperatureOnMars = -341;
+
+            string myName = "Natalia";
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+
+
+            bool isRaining = true;
+            string rainingStatus = Convert.ToString(isRaining);
+            Console.WriteLine(rainingStatus);
+            Console.ReadLine();
+
+
+            Console.WriteLine(questionmark);
+            Console.ReadLine();
+
+                
+        }
+    }       
+}
