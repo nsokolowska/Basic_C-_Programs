d59949a78cec434374d4750fdf1021e94d7c9720
nsokolowska <natalia.sokolowska12@gmail.com>
2023-03-05 20:08:40 +0000

    Create Program.cs

---
+++ Boolean Logic Assignment/Boolean Logic Assignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Logic_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Questions
+            Console.WriteLine("What is your age?");
+            string Age = Console.ReadLine();
+
+            Console.WriteLine("Have you ever had a DUI violation?   [i.e., for driving under the influence of alcohol]");
+            string DUIstring = Console.ReadLine();
+            bool DUI = bool.Parse(DUIstring);
+
+
+
+            Console.WriteLine("How many speeding tickets have you had?");
+            string Tickets = Console.ReadLine();
+
+            //Boolen
+
+            bool Qualified = (Convert.ToInt32(Age) > 15 && Convert.ToInt32(Tickets) < 3 != DUI);
+            Console.WriteLine("Qualified?");
+            Console.WriteLine(Qualified);
+            Console.ReadLine();
+
+        }
+    }
+}
