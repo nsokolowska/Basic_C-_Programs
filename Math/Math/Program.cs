e3c50acc3f32015c3fccfd7d281d4890aa84af1a
nsokolowska <natalia.sokolowska12@gmail.com>
2023-03-05 18:21:12 +0000

    Create Program.cs

---
+++ Math/Math/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Math
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Addition
+            int num1 = 5;
+            int num2 = 7;
+            int total = num1 + num2;
+            Console.WriteLine(total);
+            Console.ReadLine();
+
+            //Subtraction
+            int num3 = 6;
+            int num4 = 7;
+            int diffrence = num3 - num4;
+            Console.WriteLine(diffrence);
+            Console.ReadLine();
+
+            //Multiplication
+            int num5 = 5;
+            int num6 = 2;
+            int product = num5 * num6;
+            Console.WriteLine(product);
+            Console.ReadLine();
+
+            //Division
+            int num7 = 10;
+            int num8 = 2;
+            int quotient = num7 / num8;
+            Console.WriteLine(quotient);
+            Console.ReadLine();
+
+            //Remainder
+            int num9 = 11;
+            int num10 = 3;
+            int remainder = num9 % num10;
+            Console.WriteLine(remainder);
+            Console.ReadLine();
+
+            //
+
+        }
+    }
+}
