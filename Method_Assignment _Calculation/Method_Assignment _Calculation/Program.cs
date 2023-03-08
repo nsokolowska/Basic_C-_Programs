7c5424d51bb269e4db2832095c6d962264320e37
nsokolowska <natalia.sokolowska12@gmail.com>
2023-03-08 13:40:22 +0000

    Create Program.cs

---
+++ Method_Assignment _Calculation/Method_Assignment _Calculation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment__Calculation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello, this is a little calucation game \n Please enter your numer and wait to see what happen");
+            try
+            {
+                int UserInput = Convert.ToInt32(Console.ReadLine());
+                Calculation.MathMulti(UserInput);
+                Calculation.CalculationWithDate(UserInput);
+                Calculation.RemainderMath(UserInput);
+            }
+            catch (FormatException)
+            {
+               Console.WriteLine("You have to enter the number");
+                Console.ReadLine();
+                return;
+            }
+            catch (DivideByZeroException)
+            {
+               Console.WriteLine("You cannot enter zero value");
+                Console.ReadLine();
+                return;
+            }
+            
+        }   
+
+       
+    }
+}
