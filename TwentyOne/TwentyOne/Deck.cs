30f852c12f01ff970f6e498cd9177e0c40d68797
nsokolowska <natalia.sokolowska12@gmail.com>
2023-03-08 10:06:28 +0000

    Create Deck.cs

---
+++ TwentyOne/TwentyOne/Deck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwentyOne
+{
+    public class Deck
+    {
+        public Deck()
+        {
+            Cards = new List<Card>();
+            List<string> Sutis = new List<string>() { "Clubs", "Hearts", "Diamonds", "Spades" };
+            List<string> Faces = new List<string>
+            {
+                "Two", "Three", "Fours", "Five", "Six", "Seven", 
+                "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"
+            };
+
+            foreach(string face in Faces)
+            {
+                foreach (string suit in Sutis)
+                {
+                    Card card = new Card();
+                    card.Suit = suit;
+                    card.Face = face;
+                    Cards.Add(card);
+                }
+            }
+
+
+        }
+        public List<Card> Cards { get; set; }
+    }
+}
