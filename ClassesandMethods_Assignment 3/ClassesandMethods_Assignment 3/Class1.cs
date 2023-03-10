32bedae80835a75e325c11657ec4017304a983fa
nsokolowska <natalia.sokolowska12@gmail.com>
2023-03-10 20:31:26 +0000

    Create Class1.cs

---
+++ ClassesandMethods_Assignment 3/ClassesandMethods_Assignment 3/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesandMethods_Assignment_3
+{
+    public class Class1
+    {
+        public static void MathOperation(int num1, int num2)
+        {
+            int total = (num1 *5) / 3;
+            Console.WriteLine("total: " + total + " num2: " + num2);
+            Console.ReadLine();
+        }
+
+        
+    }
+}
