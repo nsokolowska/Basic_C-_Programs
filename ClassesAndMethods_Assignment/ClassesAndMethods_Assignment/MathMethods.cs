2827a39ef9870a07bf689ce0d53d7072eda45050
nsokolowska <natalia.sokolowska12@gmail.com>
2023-03-10 21:20:51 +0000

    Update MathMethods.cs

--- ClassesAndMethods_Assignment/ClassesAndMethods_Assignment/MathMethods.cs
+++ ClassesAndMethods_Assignment/ClassesAndMethods_Assignment/MathMethods.cs
@@ -8,28 +8,28 @@ namespace ClassesAndMethods_Assignment
 {
     public class MathMethods
     {
-        public int Method1(int uNumber)
+        public int Method1(int intNumber)
         {
             int myNumber = 345;
-            int result = myNumber + Convert.ToInt32(uNumber);
+            int result = myNumber + Convert.ToInt32(intNumber);
             Console.WriteLine(result);
             Console.ReadLine();
             return result;
         }
 
-        public decimal Method1(decimal uNumber)
+        public decimal Method1(decimal decimalNumber)
         {
             int myNumber1 = 2000;
-            int result = (int)(myNumber1 - Convert.ToDecimal(uNumber));
+            int result = (int)(myNumber1 - Convert.ToDecimal(decimalNumber));
             Console.WriteLine(result);
             Console.ReadLine();
             return result;
         }
 
-        public string Method1(string uNumber)
+        public string Method1(string stringNumber)
         {
             int myNumber = 25;
-            int newNumber = Convert.ToInt32(uNumber);
+            int newNumber = Convert.ToInt32(stringNumber);
             int firstresult = myNumber * newNumber;
             string result = firstresult.ToString();
             Console.WriteLine(result);
