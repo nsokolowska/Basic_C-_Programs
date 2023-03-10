6604b8db1324fe3a8020acc9c7642c07b6ab8621
nsokolowska <natalia.sokolowska12@gmail.com>
2023-03-10 12:01:04 +0000

    Create Program.cs

---
+++ ClassesandMethods_Assignment2/ClassesandMethods_Assignment2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesandMethods_Assignment2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, this is simple program. \n Please enter two numbers");
+            Console.ReadLine();
+            Console.WriteLine("You don't need to enter second number if you don't want to");
+            Class1.Add(Convert.ToInt32(Console.ReadLine()), Console.ReadLine());
+          
+        }
+    }
+}
