30c2c16d883de0a4159e51c1010d0fdf3a18e323
nsokolowska <natalia.sokolowska12@gmail.com>
2023-03-10 12:00:40 +0000

    Create Class1.cs

---
+++ ClassesandMethods_Assignment2/ClassesandMethods_Assignment2/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesandMethods_Assignment2
+{
+    class Class1
+    {
+        public static int Add(int num1, params string[] num2)
+        {
+            int total = num1;
+            foreach (string number in num2)
+            {
+                int i = Convert.ToInt32(number);
+                total +=i ;
+            };
+            
+            Console.WriteLine("This is " + total + " in total");
+            Console.ReadLine();
+            return total;
+        }
+    }
+}
