5cc9668316a1abd88b87cc416b627e66ab137c74
nsokolowska <natalia.sokolowska12@gmail.com>
2023-03-11 11:40:11 +0000

    Update Program.cs

--- TwentyOne/TwentyOne/Program.cs
+++ TwentyOne/TwentyOne/Program.cs
@@ -11,8 +11,9 @@ namespace TwentyOne
         static void Main(string[] args)
         {
             Deck deck = new Deck();
-            
-            deck = Shuffle(deck, 3);
+
+            deck.Shuffle(3);
+
             foreach(Card card in deck.Cards)
             {
                 Console.WriteLine(card.Face + " of " + card.Suit);
@@ -21,28 +22,16 @@ namespace TwentyOne
             Console.ReadLine();
            
         }
-        public static Deck Shuffle(Deck deck)
-        {
-            List<Card> TempList = new List<Card>();
-            Random random = new Random();
-
-            while (deck.Cards.Count > 0)
-            {
-                int randomIndex = random.Next(0, deck.Cards.Count);
-                TempList.Add(deck.Cards[randomIndex]);
-                deck.Cards.RemoveAt(randomIndex);
-            }
-            deck.Cards = TempList;
-            return deck;
-
-        }
-        public static Deck Shuffle(Deck deck, int times)
-        {
-            for (int i = 0; i < times; i++)
-            {
-                deck = Shuffle(deck);
-            }
-            return deck;
-        }
+        
+        //public static Deck Shuffle(Deck deck,int timesShuffled)
+        //{
+        //    timesShuffled = 0;
+        //    for (int i = 0; i < Shuffledtimes; i++)
+        //    {
+        //        timesShuffled++;
+        //        deck = Shuffle(deck);
+        //    }
+        //    return deck;
+        //}
     }
 }
