84ce1736424b9ad50974ce403d2f16f3f07ed5a8
nsokolowska <natalia.sokolowska12@gmail.com>
2023-03-11 10:53:46 +0000

    Update Program.cs

--- ClassesandMethods_Assignment 3/ClassesandMethods_Assignment 3/Program.cs
+++ ClassesandMethods_Assignment 3/ClassesandMethods_Assignment 3/Program.cs
@@ -10,10 +10,15 @@ namespace ClassesandMethods_Assignment_3
     {
         static void Main(string[] args)
         {
+            //user input
             int num1 = Convert.ToInt32(Console.ReadLine());
             int num2 = Convert.ToInt32(Console.ReadLine());
             Class1 newClass = new Class1();
             newClass.MathOperation(num1, num2);
+
+            //calling parameters by name:
+            newClass.MathOperation(num1: 23, num2: 342);
+
         }
     }
 }
