3c8a26e92349a5a419ee812b1749cf171eacea80
nsokolowska <natalia.sokolowska12@gmail.com>
2023-03-11 11:39:33 +0000

    Create Calculation.cs

---
+++ Class_Method_Assignment/Class_Method_Assignment/Calculation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Assignment
+{
+    public static class Calculation
+    {
+        public static void Division(int number)
+        {
+            int total = number / 2;
+            Console.WriteLine("The number: " + number + " divied by 2 equals " + total);
+            Console.ReadLine();
+            Calculation.NextOperation(total);
+        }
+        public static void NextOperation(int total)
+        {
+            int result = (total + 34) / 4;
+            Console.WriteLine("The result previous calculation plus thirty four and divided by four equals  " + result);
+            Console.ReadLine();
+            
+        }
+    }
+}
