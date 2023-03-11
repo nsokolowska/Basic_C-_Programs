27e56e1ecb725e1253722918264a3d5effc9de39
nsokolowska <natalia.sokolowska12@gmail.com>
2023-03-11 11:39:53 +0000

    Create Program.cs

---
+++ Class_Method_Assignment/Class_Method_Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Assignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //getting input from user
+            Console.WriteLine("Please enter the number which system will you to its calculation");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Calculation.Division(number);
+            
+        }
+        
+    }
+}
