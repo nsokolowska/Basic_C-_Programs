7ebfe42e3735ca75fe3c8d953ceb9f9c73d27840
nsokolowska <natalia.sokolowska12@gmail.com>
2023-03-12 09:24:41 +0000

    Create Program.cs

---
+++ Abstract_Classes Assignment/Abstract_Classes Assignment/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract_Classes_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.firstName = "Sample";
+            employee.lastName = "Student";
+            employee.SayName();
+
+        }
+    }
+}
