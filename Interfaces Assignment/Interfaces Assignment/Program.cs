ee7ebeedb2f6f6a5cb33fb3b29e120463a71dd2d
nsokolowska <natalia.sokolowska12@gmail.com>
2023-03-12 11:47:00 +0000

    Create Program.cs

---
+++ Interfaces Assignment/Interfaces Assignment/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.SayName();
+
+            Dismissed employeeDismissed = new Dismissed();
+            employeeDismissed.Quit();
+
+            Console.ReadLine();
+        }
+    }
+}
