6f7f0a73989c667081b35abe9bf9c29725e76781
nsokolowska <natalia.sokolowska12@gmail.com>
2023-03-12 11:46:12 +0000

    Create Dismissed.cs

---
+++ Interfaces Assignment/Interfaces Assignment/Dismissed.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces_Assignment
+{
+    public class Dismissed : Employee, IQuittable
+    {
+        public string Reason { get; set; }
+        public int LenghtOfService { get; set; }
+        public List<string> employeesDismissed { get; set; }
+        
+        void Quit()
+        {
+            foreach (string employeeDismissed in employeesDismissed) {
+                Console.WriteLine("Employee: " + FirstName + " " + LastName + "has left their job because " + Reason + " after " + LenghtOfService + " months of their service.");
+                Console.ReadLine();
+            }
+        }
+        
+
+    }
+}
