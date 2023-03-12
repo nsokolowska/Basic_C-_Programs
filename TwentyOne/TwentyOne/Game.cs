5191026589b4cf5e25bd5d43bb95444b32b74a77
nsokolowska <natalia.sokolowska12@gmail.com>
2023-03-12 18:44:07 +0000

    Update Game.cs

--- TwentyOne/TwentyOne/Game.cs
+++ TwentyOne/TwentyOne/Game.cs
@@ -8,16 +8,16 @@ namespace TwentyOne
 {
     public abstract class Game
     {
-        public List<string> Players { get; set; }
+        public List<Player> Players { get; set; }
         public string Name { get; set; }
         public string Dealer { get; set; }
 
         public abstract void Play();
         public virtual void ListPlayers()
         {
-            foreach (string Player in Players)
+            foreach(Player Player in Players)
             {
-                Console.WriteLine(Player);
+                Console.WriteLine(Player.Name);
             }
         }
     }
