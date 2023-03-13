ead2518aaf756e05f565cc5adf4a78936cc44d6d
nsokolowska <natalia.sokolowska12@gmail.com>
2023-03-13 15:01:37 +0000

    Update Program.cs

--- Class_Method_Assignment/Class_Method_Assignment/Program.cs
+++ Class_Method_Assignment/Class_Method_Assignment/Program.cs
@@ -11,11 +11,28 @@ namespace Class_Method_Assignment
         public static void Main(string[] args)
         {
             //getting input from user
-            Console.WriteLine("Please enter the number which system will you to its calculation");
+            Console.WriteLine("Please enter the whole number which system will you to its calculation");
             int number = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Please enter the decimal number which system will you to its calculation");
+            decimal decimalNumber = Convert.ToDecimal(Console.ReadLine());
+
+            //Calculation result = new Calculation();
             Calculation.Division(number);
+            Calculation.Division(decimalNumber);
             
+            Console.WriteLine("As remainder you entered: " + number);
+            Calculation.NewOperation(out number);
+            Console.WriteLine("The output number is: " + number);
+            Console.ReadLine();
+
+            Console.WriteLine("As remainder you entered: " + decimalNumber);
+            Calculation.NewOperation(out decimalNumber);
+            Console.WriteLine("The output number is: " + decimalNumber);
+            Console.ReadLine();
+
+
+
         }
         
     }
